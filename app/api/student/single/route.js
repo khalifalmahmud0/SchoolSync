c7dd7e4503c7aeb3daf4a433bbef1430cedd57d3
khalifalmahmud0 <code.khalif@gmail.com>
2024-01-03 11:29:35 +0600

    Uncomment Single Page API

--- app/api/student/single/route.js
+++ app/api/student/single/route.js
@@ -1,27 +1,27 @@
-// import { NextResponse } from "next/server";
-// import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-// export const GET = async (req, res) => {
-//   try {
-//     let { searchParams } = new URL(req.url);
-//     let id = parseInt(searchParams.get("id"));
+export const GET = async (req, res) => {
+  try {
+    let { searchParams } = new URL(req.url);
+    let id = parseInt(searchParams.get("id"));
 
-//     let PRISMA = new PrismaClient();
-//     let result = await PRISMA.users.findFirst({
-//       where: { id },
-//     });
+    let PRISMA = new PrismaClient();
+    let result = await PRISMA.users.findFirst({
+      where: { id },
+    });
 
-//     PRISMA.$disconnect(); // Close the Prisma client connection
-//     return NextResponse.json({
-//       status: "Success",
-//       data: result,
-//     });
-//   } catch (error) {
-//     // Handle errors
-//     console.error("Error:", error);
-//     return NextResponse.error({
-//       status: "Error",
-//       message: "An error occurred while processing the request.",
-//     });
-//   }
-// };
+    PRISMA.$disconnect(); // Close the Prisma client connection
+    return NextResponse.json({
+      status: "Success",
+      data: result,
+    });
+  } catch (error) {
+    // Handle errors
+    console.error("Error:", error);
+    return NextResponse.error({
+      status: "Error",
+      message: "An error occurred while processing the request.",
+    });
+  }
+};
